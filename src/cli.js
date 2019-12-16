6d574311777e427137ec404893904ed30002837a
Vetési Zoltán <vetesi.zoltan@gmail.com>
2019-12-16 18:45:14 +0100

    ...

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
+const NODE_MAJOR_VERSION = Number(process.versions.node.split("."))
+
 require("child_process").spawn(
     process.execPath,
     [
+        "--no-warnings",
         "--experimental-modules",
-        "--es-module-specifier-resolution=node",
+        (NODE_MAJOR_VERSION >= 12 ? "--es-module-specifier-resolution=node" : null),
         require("path").join(__dirname, "cli.mjs"),
         ...process.argv.slice(2)
-    ],
+    ].filter(v => !!v),
     { stdio: "inherit" })
