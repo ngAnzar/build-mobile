a0dae93027cfe11ffa9320825058dd2205865b8f
Vetési Zoltán <zozzz@trigon.hu>
2019-12-16 13:42:41 +0100

    ...

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ require("child_process").spawn(
     process.execPath,
     [
         "--experimental-modules",
+        "--es-module-specifier-resolution=node",
         require("path").join(__dirname, "cli.mjs"),
         ...process.argv.slice(2)
     ],
