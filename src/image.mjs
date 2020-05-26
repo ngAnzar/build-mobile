37fbd78b0a097ca84879450b13454591d2c20b48
Vetési Zoltán <vetesi.zoltan@gmail.com>
2020-05-26 16:19:21 +0200

    ...

--- src/image.mjs
+++ src/image.mjs
@@ -33,7 +33,7 @@ export async function resizeImage(inputFile, outFolder, definitions) {
 }
 
 
-async function _createSplash(imageData, outputFile, w, h, bg) {
+async function _createSplash(imageData, w, h, bg) {
     let image = await Jimp.create(w, h, bg)
     let original = await Jimp.read(imageData)
     if (original.getWidth() > w || original.getHeight() > h) {
@@ -41,7 +41,36 @@ async function _createSplash(imageData, outputFile, w, h, bg) {
     }
 
     await image.composite(original, (w - original.getWidth()) / 2, (h - original.getHeight()) / 2)
-    await image.quality(100).writeAsync(outputFile)
+    return image
+}
+
+
+async function _createNinePatchSplash(imageData, w, h, bg) {
+    let image = await Jimp.create(w + 2, h + 2, 0)
+    // top left
+    await image.setPixelColor(0x000000FF, 0, 1)
+    await image.setPixelColor(0x000000FF, 1, 0)
+
+    // top right
+    await image.setPixelColor(0x000000FF, w, 0)
+
+    // bottom left
+    await image.setPixelColor(0x000000FF, 0, h)
+
+    // right
+    for (let y = 1; y < h + 1; y++) {
+        await image.setPixelColor(0x000000FF, w + 1, y)
+    }
+
+    // bottom
+    for (let x = 1; x < w + 1; x++) {
+        await image.setPixelColor(0x000000FF, x, h + 1)
+    }
+
+    let scaleable = await _createSplash(imageData, w, h, bg)
+    await image.composite(scaleable, 1, 1)
+
+    return image
 }
 
 
@@ -57,7 +86,10 @@ export async function createSplashScreen(inputFile, outFolder, definitions, back
         const suffix = def.suffix || ""
         const outName = def.name ? def.name : `${prefix}${name}${suffix}${ext}`
         const outPath = path.join(outFolder, outName)
-        await _createSplash(imageData, outPath, def.width, def.height, backgroundColor)
+        const image = def.ninePatch
+            ? await _createNinePatchSplash(imageData, def.width, def.height, backgroundColor)
+            : await _createSplash(imageData, def.width, def.height, backgroundColor)
+        await image.quality(100).writeAsync(outPath)
         result.push({ path: outPath, variant: def })
     }
 
--- src/splashscreen.mjs
+++ src/splashscreen.mjs
@@ -3,18 +3,25 @@
 export const ANDROID_SPLASH = {
     path: "res/splash/android",
     variants: [
-        { name: "land-ldpi.png", density: "land-ldpi", width: 320, height: 200 },
-        { name: "land-mdpi.png", density: "land-mdpi", width: 480, height: 320 },
-        { name: "land-hdpi.png", density: "land-hdpi", width: 800, height: 480 },
-        { name: "land-xhdpi.png", density: "land-xhdpi", width: 1280, height: 720 },
-        { name: "land-xxhdpi.png", density: "land-xxhdpi", width: 1600, height: 960 },
-        { name: "land-xxxhdpi.png", density: "land-xxxhdpi", width: 1920, height: 1280 },
+        // { name: "land-ldpi.png", density: "land-ldpi", width: 320, height: 200 },
+        // { name: "land-mdpi.png", density: "land-mdpi", width: 480, height: 320 },
+        // { name: "land-hdpi.png", density: "land-hdpi", width: 800, height: 480 },
+        // { name: "land-xhdpi.png", density: "land-xhdpi", width: 1280, height: 720 },
+        // { name: "land-xxhdpi.png", density: "land-xxhdpi", width: 1600, height: 960 },
+        // { name: "land-xxxhdpi.png", density: "land-xxxhdpi", width: 1920, height: 1280 },
 
-        { name: "port-hdpi.png", density: "port-hdpi", width: 200, height: 320 },
-        { name: "port-ldpi.png", density: "port-ldpi", width: 320, height: 480 },
-        { name: "port-mdpi.png", density: "port-mdpi", width: 480, height: 800 },
-        { name: "port-xhdpi.png", density: "port-xhdpi", width: 720, height: 1280 },
-        { name: "port-xxhdpi.png", density: "port-xxhdpi", width: 960, height: 1600 },
-        { name: "port-xxxhdpi.png", density: "port-xxxhdpi", width: 1280, height: 1920 },
+        // { name: "port-hdpi.png", density: "port-hdpi", width: 200, height: 320 },
+        // { name: "port-ldpi.png", density: "port-ldpi", width: 320, height: 480 },
+        // { name: "port-mdpi.png", density: "port-mdpi", width: 480, height: 800 },
+        // { name: "port-xhdpi.png", density: "port-xhdpi", width: 720, height: 1280 },
+        // { name: "port-xxhdpi.png", density: "port-xxhdpi", width: 960, height: 1600 },
+        // { name: "port-xxxhdpi.png", density: "port-xxxhdpi", width: 1280, height: 1920 },
+
+        { name: "ldpi.9.png", density: "ldpi", width: 200, height: 200, ninePatch: true },
+        { name: "mdpi.9.png", density: "mdpi", width: 320, height: 320, ninePatch: true },
+        { name: "hdpi.9.png", density: "hdpi", width: 480, height: 480, ninePatch: true },
+        { name: "xhdpi.9.png", density: "xhdpi", width: 720, height: 720, ninePatch: true },
+        { name: "xxhdpi.9.png", density: "xxhdpi", width: 960, height: 960, ninePatch: true },
+        { name: "xxxhdpi.9.png", density: "xxxhdpi", width: 1280, height: 1280, ninePatch: true },
     ]
 }
